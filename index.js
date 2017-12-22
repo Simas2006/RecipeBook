26e1a0f96fab415f2e8fc37183ae223868caee4a
BuildTools <sjuknelis@gmail.com>
2017-12-22 17:49:26 -0500

    Main server code

---
+++ index.js
@@ -0,0 +1,28 @@
+var requestLib = require("request");
+var fs = require("fs");
+var express = require("express");
+var app = express();
+var PORT = process.argv[2] || 8000;
+
+app.use("/recipes",express.static(__dirname + "/recipes"));
+app.use("/public",express.static(__dirname + "/public"));
+app.get("/",function(request,response) {
+  response.send("<html><body><script>location.href = \"/public/index.html\";</script></body></html>");
+});
+app.get("/register",function(request,response) {
+  var qs = decodeURIComponent(request.url.split("?").slice(1).join("?")).split(",");
+  console.log(qs);
+  requestLib(qs[0],function(err,didRespond,body) {
+    if ( err ) throw err;
+    if ( didRespond ) {
+      fs.writeFile(__dirname + "/recipes/" + qs[1] + ".html",body,function(err) {
+        if ( err ) throw err;
+        response.send("ok");
+      });
+    }
+  })
+});
+
+app.listen(PORT,function() {
+  console.log("Listening on port " + PORT);
+})
